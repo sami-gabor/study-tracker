7bf7ff6d73cb9e359cee3ca30f70a8e804d1f2f7
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-03-09 16:25:22 +0200

    Change request through Interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,6 +3,11 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Interceptor sent with every request...');
-        return next.handle(req);
+
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'xyz')
+        });
+
+        return next.handle(modifiedRequest);
     }
 }
