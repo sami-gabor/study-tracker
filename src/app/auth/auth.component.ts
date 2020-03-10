02a03caa233d7f8629f0f9b46745b6ac7953597a
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-03-10 16:51:29 +0200

    Add form validation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -11,6 +12,11 @@ export class AuthComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('form: ', form.value);
+    form.reset();
   }
 
   onSwitchMode() {
