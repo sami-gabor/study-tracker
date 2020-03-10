968a487fbf0d3636faed0dc3f1e5f070ced878c7
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-03-10 09:42:50 +0200

    Add loading wheel for students component

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
 import { StudentsSetvice } from '../students.service';
 
 import { Student } from 'src/interfaces/student.interface';
 import { FirebaseStudentsService } from '../firebase-students.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 
 @Component({
@@ -12,17 +14,25 @@ import { FirebaseStudentsService } from '../firebase-students.service';
 })
 export class StudentsComponent implements OnInit {
   students: Student[];
+  isLoading: boolean;
 
   constructor(
+    private ngxService: NgxUiLoaderService,
     private studentsService: StudentsSetvice,
     private firebaseStudentsService: FirebaseStudentsService
   ) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.ngxService.start();
+
     this.firebaseStudentsService.fetchStudents().subscribe(students => {
       this.students = students;
       this.studentsService.students = students;
       this.studentsService.sortStudents();
+
+      this.isLoading = false;
+      this.ngxService.stop();
     });
   }
 
