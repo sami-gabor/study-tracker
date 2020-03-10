73d9d0c02417d0c60792c3cec69c19d43dfc2e6f
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-03-10 15:46:15 +0200

    Add Switch button functionality

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
 
 }
